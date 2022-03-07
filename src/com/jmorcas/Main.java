424e1e48ead347d668fbaeeb7359d0a67ecc7637
agus <abarlar1812@g.educaand.es>
2022-03-07 16:18:59 +0100

    Tercer commit

--- src/com/jmorcas/Main.java
+++ src/com/jmorcas/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, somos juan y agustin");
         System.out.println("Hoy es 07/03/2022");
+        System.out.println("Escrito por Agustin");
 
     }
 }
